85cce2c6ee23a22fec7e899f26b8f486473a15c6
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-10-01 22:05:04 +0300

    Create GamePage.js

---
+++ src/pages/GamePage.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState } from 'react';
+import ActionInfo from '../components/ActionsInfo';
+import BoardComponent from '../components/BoardComponent';
+import { Board } from '../' 
+
+const GamePage = () => {
+
+    const {gameId} = useParams()
+    
+    return (
+        <div>
+            <p> chto-to na arm9nskom </p>
+            <div className='boards-container'>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default GamePage;

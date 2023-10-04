66e71b174dbce04a587bd0831a85ff7c81792ab3
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-10-04 13:45:17 +0300

    Update BoardComponent.js

---
+++ src/components/BoardComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const BoardComponent = ({board, setBoard, shipsReady, isMyBoard, canShoot, shoot}) => {
+
+    const boardClasses = ['board']
+
+    if(canShoot) {
+        boardClasses.push('active-shoot')
+    }
+
+
+    return (
+        <div className={boardClasses.join(' ')}>
+
+        </div>
+    
+
+    );
+}
+
+export default BoardComponent;

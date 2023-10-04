519f3a6e073a25360c575ef4e534cfe3d1c723c9
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-10-04 20:15:37 +0300

    Update BoardComponent.js

--- src/components/BoardComponent.js
+++ src/components/BoardComponent.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import CellComponent from './CellComponent';
 const BoardComponent = ({board, setBoard, shipsReady, isMyBoard, canShoot, shoot}) => {
 
     const boardClasses = ['board']
 
+    function addMark(x, y) {
+        if(!shipsReady && isMyBoard) {
+            board.assShip(x, y)
+        } else if (canShoot && !isMyBoard) {
+            shoot(x,y)
+        }
+
+        updateBoard()
+    }
+
+    function updateBoard() {
+        const newBoard = board.getCopyBoard()
+
+        setBoard(newBoard)
+    }
     if(canShoot) {
         boardClasses.push('active-shoot')
     }
@@ -10,6 +26,14 @@ const BoardComponent = ({board, setBoard, shipsReady, isMyBoard, canShoot, shoot
 
     return (
         <div className={boardClasses.join(' ')}>
+            {board.cells.map((row, index) => 
+             <React.Fragment key={index}>
+                {row.map(cell => 
+                    <CellComponent key={cell.id} cell={cell} addMark={addMark}/>
+                    
+                    )}
+             </React.Fragment>
+            )}
 
         </div>
     

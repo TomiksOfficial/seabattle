141405c8034b6a7a2424d65d7c38d207864116cd
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-10-04 21:26:21 +0300

    Update Ship.js

--- public/models/marks/Ship.js
+++ public/models/marks/Ship.js
@@ -1,7 +1,8 @@
 import { Mark } from "./Mark";
 
 export class Ship extends Mark {
-    super(cell); {
+    constructor(cell) {
+    super(cell); 
     this.logo = null;
     this.name = 'ship'
     this.color = 'grey'

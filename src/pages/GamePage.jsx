1f5fa454e703f752788567a86d1f5fc6cc464fd8
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-10-04 20:15:33 +0300

    Update GamePage.jsx

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -4,18 +4,53 @@ import BoardComponent from '../components/BoardComponent';
 import { Board } from '..' 
 
 const GamePage = () => {
+    const[myBoard, setMyBoard] = useState(new Board());
+    const[hisBoard, setHisBoard] = useState(new Board());
+    const[rivalName, setRivalName] = useState('')
+    const[shipsReady, setShipsReady] = useState(false)
+    const[canShoot, setCanShoot] = useState(false)
+    const {gameId} = useParams() 
 
-    const {gameId} = useParams()
+    function restart() {
+        const newMyBoard = new Board()
+        const newHisBoard = new Board()
+        newMyBoard.inintCells()
+        newHisBoard.inintCells()
+        setMyBoard(newMyBoard)
+        setHisBoard(newHisBoard)
+    }
+
+    function shoot(x, y) {
+
+    }
+
+    useEffect(() => {
+        restart()
+    }, [])
     
     return (
         <div>
             <p> chto-to na arm9nskom </p>
             <div className='boards-container'>
                 <div>
-
+                    <p className='nick'>{localStorage.nickname}</p>
+                    <BoardComponent
+                         board={myBoard}
+                         isMyBoard
+                         shipsready={shipsReady}
+                         setBoard={setMyBoard}
+                         canShoot={false}
+                    />
                 </div>
                 <div>
-
+                    <p className='nick'>{rivalName || 'Poka neizvesten vrag'}</p>
+                    <BoardComponent
+                         board={hisBoard}
+                         setBoard={setHisBoard}
+                         canShoot={canShoot}
+                         shipsReady={shipsReady}
+                         shoot={shoot}
+                    />
                 </div>
             </div>
         </div>

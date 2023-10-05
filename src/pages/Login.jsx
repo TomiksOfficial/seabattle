2a3b524faf4925fbb188e9f7926996788ecaf535
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-10-05 16:33:16 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Login = {} => {
+const Login = () => {
     const [invitationGame, setInvitationGame] = useState()
     const [gameId, setGameId] = useState('');
     const [nickname, setNickname] = useState('');
@@ -23,9 +23,63 @@ const Login = {} => {
                     <input
                       type="text"
                       name="nickname"
-                      id=
+                      id="nickname"
+                      onChange={e => setNickname(e.target.value)} />
+                </div>
+                <div onChange={(e) => setInvitationGame(e.target.id === 'ingame')} className="field-group">
+                    <input
+                     tupe="radio"
+                     name="typeEnter"
+                     id="gen"
+                     value={!invitationGame}
+                     defaultChecked={!invitationGame}
 
+                     />
+                     <label htmlFor="gen"> Создать игру </label>
+                     <input
+                     tupe="radio"
+                     name="typeEnter"
+                     id="ingame"
+                     value={invitationGame}
+                     defaultChecked={invitationGame}
+
+                     />
+                    <label htmlFor="ingame"> Войти по идентификатору </label>
+                </div>
+                <div className="field-group">
+                     {invitationGame ? (
+                        <>
+                          <div>
+                            <label htmlFor="gameId"> Введите идентификатор игры </label>
+                          </div>
+                          <input
+                          tupe="rext"
+                          name="gameId"
+                          defaultValue=""
+                          id="gameId"
+                          onChange={e => setGameId=(e.target.value)} />
+                          </>
+
+                     ) : (
+                        <>
+                          <button
+                            className="btn-gen"
+                            onClick={(e) => {
+                                e.preventDefault()
+                                setGameId(Date.now())
+                            }}>
+                            Сгенерировать идентификатор игры
+                            </button>
+                            <p>{gameId}</p>
+                        </>     
+
+                    )}
+                </div>
+                
+                <button type="submit" className="btn-ready"> ГИРАТЬ </button>
             </form>
         </div>
-    )
+    );
 }
+
+export default Login;

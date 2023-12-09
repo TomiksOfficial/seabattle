23ede8c0f89b501d80de9934e2fa571f62b72c68
Br0k0da <danilkokosha@gmail.com>
2023-12-09 21:05:54 +0300

    fix

--- src/components/Battlefields/FieldOne/CheckeredFieldOne/Checker.jsx
+++ src/components/Battlefields/FieldOne/CheckeredFieldOne/Checker.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import classes from './Checker.module.css'
 
 const Checker = (props) => {
     return <div style={{background:props.ship_setted}} onClick={() => props.callback()}></div>;
